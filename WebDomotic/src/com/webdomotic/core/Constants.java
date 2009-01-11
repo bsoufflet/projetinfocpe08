6006fe4b9b3f1f89ba5bc069c16f268b66245a8c
Paulfruhauf <Paulfruhauf@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-11 19:10:48 +0000

    new JNDI name for DB

--- WebDomotic/src/com/webdomotic/core/Constants.java
+++ WebDomotic/src/com/webdomotic/core/Constants.java
@@ -6,8 +6,10 @@ public interface Constants {
 	/**
 	 * DB properties
 	 */
+	final static String DBurl = "jdbc:mysql://localhost:3306/webdomotic";
+	final static String DBuser = "root";
+	final static String DBpass = null;
 	final static String DB_JNDI_NAME = "MySqlDS";
-	
 	
 	
 	/**
