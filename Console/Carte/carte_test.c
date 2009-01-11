d31bbeac2c1ee1de3deae031832079cc8a2958af
marina.gousseff <marina.gousseff@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-11 21:50:45 +0000

    tester avec Paul Fork a améliorer

--- Console/Carte/carte_test.c
+++ Console/Carte/carte_test.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 
 // Type bool�en
@@ -27,18 +28,24 @@ int cb (unsigned char msg,SOCKET sock)			//fonction de callback
 {
 
 	char acc;
-	int taille;
-	FILE *data;
-	char *a_mettre_ds_fichier;
+	int taille, frame;
+	char *data;
+	pid_t fils_pid;
+
+	//frame = ; //taille d'une frame socket en reception
 
 	switch (msg)
 	{
 		char mess;
+		int i, frame;
+		FILE *f_test;
+		
 		case 0 : // lors de la connexion initiale
 			
-			if(CNX_envoi_char(sock,'1'))	//accuse
-				return 1;
+			//if(CNX_envoi_char(sock,'1'))	//accuse
+			//	return 1;
 			printf("connexion msg = 0\n");
+			
 		
 			break;	// fin de l'initialisation
 
@@ -47,15 +54,48 @@ int cb (unsigned char msg,SOCKET sock)			//fonction de callback
 			// recoit les donnees de la taille precedemment envoyee
 			// mise à jour des fichiers quand ttes les donnees annoncees sont arrivees
 			printf ("dans 'd'\n");
+			f_test=fopen("ordre.txt", "w");
 			CNX_recoit_int (sock, &taille);
-			CNX_envoi_char (sock, "o");
 			printf ("taille = %d\n", taille);
+		
+		/*	
+			// decouper la taille en plusieurs frame
+			for (i=taille; i==i%frame; i= i-frame)
+			{
+				CNX_recoit (sock, data, frame);
+				printf ("%s\n", data);
+				fprintf (f_test, "/s\n", data);
+			}
+			if (i!=0)
+			{
+				CNX_recoit (sock, data, i);
+				printf ("%s\n", data);
+				fprintf (f_test, "/s\n", data);
+			}
+			fclose (f_test);*/
+
 			CNX_recoit (sock, data, taille);
+			fprintf (f_test,"%s", data);
 			printf ("%s\n", data);
+		//	CNX_envoi (sock, "coucou paul 1234\n", strlen("coucou paul 1234\n"));
+			printf ("apres envoi string\n");
+			fclose (f_test);
+		/*	fils_pid=vfork();
+			printf ("pid_fils = %d\n", fils_pid);
+			if (fils_pid > 0)	// dans le fils
+			{
+				//printf ("dans le fils\n");
+				printf ("pid_fils = %d\n", fils_pid);
+				execlp ("./parser", "./parser", NULL); //on remet le timestamp du PC dans la carte
+				_exit (3);
+			}
+			wait (NULL);*/
+			
 			break;
 
 		case 255 :		// message de fin de connexion
-			return 1;	// fin de la connexion
+			printf ("message deconnexion %i\n", msg);
+			return 1;
 			break;	
 
 		default :
@@ -80,6 +120,14 @@ int main()
 			printf ("erreur serveur\n");
 			return 1;
 		}
+		// on lance parsage fichier ici comme ca la deconnexion a eu lieu
+		// if (!fork())	//on est dans le fils
+		/*{
+			execlp ("./parser", "./parser", NULL);
+			exit (3);
+		}
+		wait (NULL);	// le pere attend le fils
+		*/
 		printf ("terminate carte\n");
 	}
 	return 0;
