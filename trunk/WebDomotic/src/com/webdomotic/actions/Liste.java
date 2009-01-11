a02054a107a195e19fa7db3867ac5c17ea1c8ee2
bsoufflet <bsoufflet@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-11 15:49:53 +0000

    trad francais de la validation. test dans liste.java pour la db. public pour serverdb

--- trunk/WebDomotic/src/com/webdomotic/actions/Liste.java
+++ trunk/WebDomotic/src/com/webdomotic/actions/Liste.java
@@ -1,8 +1,27 @@
 package com.webdomotic.actions;
 
+import com.webdomotic.core.*;
+
 public class Liste extends Vue {
+	private String listeDonnee; 
 	public String execute() throws Exception {
 		System.out.println("Liste Action - module:" + selectedModule);
+		retrouverData();
 		return SUCCESS;
+	}
+	public void retrouverData(){
+		ServerDB db = new ServerDB(Constants.DBurl,Constants.DBuser,Constants.DBpass);
+		String [][] result = db.queryDB("SELECT * FROM MAISONS");
+		
+		for(int i=0; i<result.length; i++){
+			for(int j=0; j<result[0].length; j++){
+				System.out.print(result[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	public void construireListe(){
+
+		
 	}
 }
