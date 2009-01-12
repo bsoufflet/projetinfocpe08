bae0b2289e5fd41f744067bf8762c85ed4224fbe
Paulfruhauf <Paulfruhauf@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-12 18:17:34 +0000

    new genQuery method new privilegeQuery method

--- WebDomotic/src/com/webdomotic/core/Hypervisor.java
+++ WebDomotic/src/com/webdomotic/core/Hypervisor.java
@@ -1,5 +1,9 @@
 package com.webdomotic.core;
 
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+
 public class Hypervisor {
 	private ServerDB db;
 	private String query;
@@ -18,11 +22,40 @@ public class Hypervisor {
 		return null;
 	}
 	
+    private void genQuery(String module, String action, String id){
+        query="SELECT "+getDBTableName(module)+".* FROM "+getDBTableName(module);
+        String privilegeQuery=privilegeQuery(module);
+        query+=privilegeQuery;       
+        if(id!=null && id!=""){
+            if(privilegeQuery == ""){
+                query+=" WHERE id='"+id+"'";
+            }else{
+                query+=" AND id='"+id+"'";
+            }
+        } 
+    }
+
+    private String privilegeQuery(String module){
+        Map session = ActionContext.getContext().getSession();
+        String isadmin = (String)session.get("isadmin");
+        if(isadmin == "true"){
+            String userid = (String)session.get("userid");
+            if(module == "maison"){
+                return " WHERE utilisateurs_id = '"+userid+"'";
+            }else if(module == "piece" || module == "console"){
+                return "INNER JOIN maisons ON pieces.maisons_id = maisons.id WHERE maisons.utilisateurs_id = '"+userid+"'";
+            }else if(module == "peripherique"){
+                return "INNER JOIN pieces ON peripheriques.pieces_id = pieces.id INNER JOIN maisons ON pieces.maisons_id = maisons.id WHERE maisons.utilisateurs_id = '"+userid+"'";
+            }else if(module == "utilisateur"){
+                return " WHERE id = '"+userid+"'";
+            }else{
+                return "";
+            }
+        }else{
+            return "";
+        }
+    }
 	
-	private void genQuery(String module, String action, String id){
-		query="SELECT * FROM "+getDBTableName(module);
-		
-	}
 	private void performQuery(){
 		queryResult=db.queryDB(query);
 	}
@@ -45,7 +78,6 @@ public class Hypervisor {
 		return searchMapping(input, Constants.DB_TABLE);
 	}
 
-	
 	
 	private static String searchMapping(String input,int return_type){
 		
