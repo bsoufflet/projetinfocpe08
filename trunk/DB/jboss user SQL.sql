1d7e95ad7f6ca103cbed4a6b27095067d98549de
paulfruhauf <paulfruhauf@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-13 08:57:55 +0000

    updated jboss user privileges

--- trunk/DB/jboss user SQL.sql
+++ trunk/DB/jboss user SQL.sql
@@ -1,4 +1,21 @@
+CREATE USER 'jboss'@'localhost' IDENTIFIED BY 'jboss';
 
-CREATE USER 'jboss'@'localhost';
 
-GRANT ALL PRIVILEGES ON `webdomotic` . * TO 'jboss'@'localhost' WITH GRANT OPTION ;
+GRANT SELECT ,
+INSERT ,
+
+UPDATE ,
+DELETE ,
+CREATE ,
+REFERENCES ,
+INDEX ,
+ALTER ,
+CREATE TEMPORARY TABLES ,
+CREATE VIEW ,
+SHOW VIEW ,
+CREATE ROUTINE,
+ALTER ROUTINE,
+EXECUTE ON `webdomotic` . * TO 'jboss'@'localhost';
+
+
+/*GRANT ALL PRIVILEGES ON `webdomotic` . * TO 'jboss'@'localhost' WITH GRANT OPTION ;*/
