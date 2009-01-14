ef32805d52e07384a64dde11b2f3a1fd313f6bf4
bsoufflet <bsoufflet@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-14 08:41:14 +0000

    Affichage de la liste avec YUI - premiere version fonctionnelle

--- trunk/WebDomotic/src/com/webdomotic/actions/Liste.java
+++ trunk/WebDomotic/src/com/webdomotic/actions/Liste.java
@@ -4,22 +4,22 @@ import com.webdomotic.core.*;
 
 public class Liste extends Vue {
 	
-	private String dataXML;
-	private String[][] columnDefs={{"a","b","c","d"},{"1","2","3","4"}};
+	private String dataJSArray;
+	private String[][] columnDefs;
 	
 	public String execute() throws Exception {
 		System.out.println("Liste Action - module:" + selectedModule);
-		/*Object[] result=Hypervisor.getDataXML(selectedModule, selectedAction, "", "");
-		dataXML=(String)result[0];
-		columnDefs=(String[][])result[1];*/
-		dataXML="aaaaa";
+		Object[] result=Hypervisor.getDataJSArray(selectedModule, selectedAction, "", "");
+		dataJSArray=(String)result[0];
+		System.out.println("LISTE - donnee de getDataJSArray:"+dataJSArray);
+		columnDefs=(String[][])result[1];
 		return SUCCESS;
 	}
-	public void setDataXML(String dataXML){
-		this.dataXML=dataXML;
+	public void setDataJSArray(String dataJSArray){
+		this.dataJSArray=dataJSArray;
 	}
-	public String getDataXML(){
-		return dataXML;
+	public String getDataJSArray(){
+		return dataJSArray;
 	}
 	public void setColumnDefs(String[][] columnDefs){
 		this.columnDefs=columnDefs;
