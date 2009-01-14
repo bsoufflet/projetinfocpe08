e7345df0817d0d60e620fbb80fbde8003b87c9e2
sl.tieche <sl.tieche@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-14 10:49:21 +0000

    ecriture dans fichier

--- WebDomotic/src/com/webdomotic/core/HypervisorConsole.java
+++ WebDomotic/src/com/webdomotic/core/HypervisorConsole.java
@@ -1,6 +1,10 @@
 package com.webdomotic.core;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Vector;
+import java.util.Date;
 
 public class HypervisorConsole extends Thread {
 	private static ServerDB db;
@@ -9,11 +13,21 @@ public class HypervisorConsole extends Thread {
 	}
 	
 	public static void envoyerOrdres(String userid) {
+		Date D = new Date();
 		Vector<String> t_ordres = construireOrdres(userid);
-		for(int i=0; i<t_ordres.size(); i++){
+		try {
+			FileWriter fichier = new FileWriter(userid+"_"+D.getTime()+".txt");
+			BufferedWriter out = new BufferedWriter(fichier);
+			for(int i=0; i<t_ordres.size(); i++){
 			// Envoi de l'ordre à la carte par la socket.
-			
-			
+				out.write(t_ordres.get(i));
+				out.newLine();
+				System.out.println(t_ordres.get(i));
+			}
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
@@ -21,7 +35,7 @@ public class HypervisorConsole extends Thread {
 		Vector<String> t_ordres = new Vector<String>();
 		String[][] t_regles = recupererRegles(userid);
 		int index = 0;
-		for(int i=0; i<t_regles.length; i++){
+		for(int i=1; i<t_regles.length; i++){
 			// Generation des ordres à partir des regles :
 			t_ordres.add(t_regles[i][3]); //champs description de la table
 			index++;
@@ -31,7 +45,9 @@ public class HypervisorConsole extends Thread {
 	
 	private static String[][] recupererRegles(String userid){
         String query="SELECT * FROM regles WHERE utilisateur_id = '"+userid+"'";
+        db = new ServerDB();
         String [][] result = db.queryDB(query);
+        db.close();
         return result;
     }
 	
