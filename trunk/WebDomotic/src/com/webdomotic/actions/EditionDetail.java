14eb561f032f22031c6ed218636974eec819772a
Paulfruhauf <Paulfruhauf@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-15 15:48:12 +0000

    finished and tested saveToDB()

--- trunk/WebDomotic/src/com/webdomotic/actions/EditionDetail.java
+++ trunk/WebDomotic/src/com/webdomotic/actions/EditionDetail.java
@@ -3,10 +3,19 @@ package com.webdomotic.actions;
 import com.opensymphony.xwork2.ActionContext;
 import com.webdomotic.core.*;
 
-public class EditionDetail extends Vue {
-	
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+public class EditionDetail extends Vue implements ServletRequestAware,ServletResponseAware{
+
 	private String[][] fieldToDisplay;
-	
+
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+
 	public String execute() throws Exception {
 		System.out.println("EditionDetail Action - module:" + selectedModule);
 		if(selectedModule.equals("compte") && !isAdmin.equals("true")){
@@ -20,22 +29,39 @@ public class EditionDetail extends Vue {
 		}
 		return SUCCESS;
 	}
-	
+
 	public String saveToDB(){
 		System.out.println("Action: Save");
-		if(Hypervisor.saveQuery(ActionContext.getContext().getParameters(),selectedModule))
+		if(Hypervisor.saveQuery(ActionContext.getContext().getParameters(),selectedModule)){
+			selectedAction = "detail";
 			return SUCCESS;
+		}
 		else{
 			addActionError("Erreur dans la sauvegarde");
 			return ERROR;
 		}
 	}
-	
-	
+
 	public void setFieldToDisplay(String[][] fieldToDisplay){
 		this.fieldToDisplay=fieldToDisplay;
 	}
 	public String[][] getFieldToDisplay(){
 		return fieldToDisplay;
+	}
+
+	public void setServletRequest(HttpServletRequest request){
+		this.request = request;
+	}
+
+	public HttpServletRequest getServletRequest(){
+		return request;
+	}
+
+	public void setServletResponse(HttpServletResponse response){
+		this.response = response;
+	}
+
+	public HttpServletResponse getServletResponse(){
+		return response;
 	}
 }
