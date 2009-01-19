c4c005c1c551ba29f33f34cbbb08d5ed5a4feb74
marina.gousseff <marina.gousseff@b5d4a5d4-a2b0-11dd-9bad-6bc74a82fd44>
2009-01-19 10:56:17 +0000

    tout fonctionne avec le X10 il faut tester avec le total

--- trunk/Console/parser/parser.c
+++ trunk/Console/parser/parser.c
@@ -1,10 +1,5 @@
 #include "parser.h"
-
-void int2hex(char* resultat,int valeur)
-{
-	printf ("int2hex valeur recue %d\n", valeur);
-	sprintf (resultat, "%x", valeur);
-}
+#include "x10.h"
 
 
 int main()
@@ -13,24 +8,32 @@ int main()
 	time_t t;
 	FILE * file;
 	char buffer[20],temps[20],adresse_house[20],adresse_device[20],ordre[20];
-
-	char envoi_serie1[3], envoi_serie2[3], recoit_serie;	// ce que l'on envoi sur la laison série
-	char tmp;
-	int TMP[7];
+	int retour;
+	unsigned char adresse;
+	int TMP[2];
 	int i;
-	char checksum;
-	TMP[3]=4;	//header envoi adresses
-	TMP[4]=0;	// acquittement bon checksum
-	TMP[5]=6;	// header pour la fonction
-	memcpy (&envoi_serie1[2], &TMP[4], 1);
-	printf ("dans parser\n");
+
+	printf ("dans parser toto\n");
 	
-	if ((fd=open("/dev/ttyS1",O_RDWR)) <0)	// ouverture de la liason serie sur le port COM 2!!!! (COM1 = console)
+// ouverture de la liason serie sur le port COM 2 de la carte!!!! (COM1 = console)
+	
+	retour = connexion (&fd);	// connexion de la liaison serie
+	if (retour != 0)
 	{
-		perror ("open");
-		printf("Error opening RS232\n");
-		return (-1);
+		printf ("erreur ouverture port serie\n");
+		_exit (3);
 	}
+	printf ("fd = %d\n", fd);
+	retour = parametrage (fd, B4800);	// parametrage de la vitesse du port serie (d'autre parametre sont en dur dans la fonction)
+	if (retour != 0)
+	{
+		printf ("erreur parametrage port serie\n");
+		deconnexion (fd);	// fermeture du port serie
+		_exit (3);
+	}
+	printf ("port serie correctement ouvert et parametre\n");
+	// fin reglage liaison serie
+
 
 	// action parser du fichier
 	if((file = fopen("ordres.txt", "r")) != NULL) //on ouvre le fichier et verifie si on y arrive
@@ -72,60 +75,41 @@ int main()
 			
 			strncpy(ordre, buffer+15, 1);
 			printf("ordre : %s\n", ordre);
+			TMP[1] = 16*TMP[0]+TMP[1];
+			memcpy(&adresse, &TMP[1], 1);
 			
-			TMP[2] = 16*TMP[0]+TMP[1];
-			printf ("tmp[2]: %d\n", TMP[2]);
-			memcpy(&envoi_serie1[0], &TMP[3], 1);
-			memcpy(&envoi_serie1[1], &TMP[2], 1);
-			printf ("envoye sur serie: %x %x\n", envoi_serie1[0], envoi_serie1[1]);
-
-			checksum = ((envoi_serie1[0]+envoi_serie1[1])& 0xff);
-			// a chaque ligne lu, envoi sur la liaison série
-			do
+			retour = adresse_transmit (fd, adresse);
+			if (retour != 0)
 			{
-				printf ("dans do\n");
-				write (fd, envoi_serie1, 2);	//envoi de l'entete adresse
-			//	write (fd, &envoi_serie[1], 1);	//envoi de HouseCode et DeviceCode
-				printf ("apres write\n");
-				read (fd, &recoit_serie, 1);	// lecture liaison serie pour la checksum
-				printf ("adresse recoit_serie = %x checksum = %x\n", recoit_serie, checksum);
-			
-			}while(recoit_serie != checksum);	// tant que la checksum est incorrect on renvoi l'adresse et le header
-			write (fd, &envoi_serie1[2], 1);	// on envoi OxOO au peripherique
-			read (fd, &recoit_serie, 1);	// on lit 0x55
-			printf ("recoit_serie = %x \n", recoit_serie);
-			memcpy (&envoi_serie2[0], &TMP[5], 1);
-
-			if (ordre[0] == '1')	// lampe on
-				TMP[7] = 16*TMP[0]+2;
-			if (ordre[0] == '0')	// lampe off
-				TMP[7] = 16*TMP[0]+3;
-
-			memcpy(&envoi_serie2[1], &TMP[7], 1);
-			checksum = ((envoi_serie2[0]+envoi_serie2[1])& 0xff);
-			do
+				printf ("erreur transmit adresse\n");
+				deconnexion (fd);	// fermeture du port serie
+				_exit (3);
+			}
+			if (ordre[0] == '0')	//lampe off
 			{
-				write (fd, envoi_serie2, 2);	//envoi du header pour la fonction
-			//	write (fd, &envoi_serie[1], 1);	//envoi de la fonction
-				read (fd, &recoit_serie, 1);	// lecture liaison serie
-				printf ("fonction recoit_serie = %x checksum = %x\n", recoit_serie, checksum);
-			
-			}while(recoit_serie != checksum);
-			write (fd, &envoi_serie1[2], 1);	// envoi du 0x00
-			read (fd, &recoit_serie, 1);
-			
+				retour = prise_off (fd, TMP[0]);
+				if (retour != 0)
+				{
+					printf ("erreur prise off\n");
+					deconnexion (fd);	// fermeture du port serie
+					_exit (3);
+				}
+			}
+			else if (ordre[0] == '1')
+			{
+				retour = prise_on (fd, TMP[0]);
+				if (retour != 0)
+				{
+					printf ("erreur prise on\n");
+					deconnexion (fd);	// fermeture du port serie
+					_exit (3);
+				}
+			}
 		}
-		
+
 		fclose(file);
 	}
+	deconnexion (fd);
 	_exit(3);
 	//return 0;
 }
-
-/*
-int valeur = 6328900;
-char Resultat[4];
-memcpy(Resultat, &valeur, 4);
-
-itoa (TMP[2],envoi_serie[1], 16);
-*/
